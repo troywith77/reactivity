09a2f6c76a71290a7be029a28817d66ab4e2ae6d
troywith77 <ruitang307@gmail.com>
2020-03-13 23:12:57 +0800

    intro watcher

--- src/reactivity.js
+++ src/reactivity.js
@@ -19,12 +19,16 @@ let quantity = 2
 let total = 0
 let target = null
 
-target = () => {
-  total = price * quantity
+function watcher(func) {
+  target = func
+  dep.depend()
+  target()
+  target = null
 }
 
-dep.depend()
-target()
+watcher(() => {
+  total = price * quantity
+})
 
 price = 20
 console.log(total)
